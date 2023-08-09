82ab2c275721899b00233213f8bc8f3de1cf45fc
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2023-08-09 10:07:24 +0800

    chore: add comments in lint staged rc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,13 @@
 module.exports = {
+  /**
+   * @description Auto fix code style and formatting
+   * @see https://github.com/okonet/lint-staged#reformatting-the-code
+   * @see https://github.com/okonet/lint-staged#automatically-fix-code-style-with---fix-and-add-to-commit
+   */
   '**/*.ts?(x)': ['eslint --fix', 'prettier --write'],
+  /**
+   * @description Run tsc on changes to TypeScript files, but do not pass any filename arguments
+   * @see https://github.com/okonet/lint-staged#example-run-tsc-on-changes-to-typescript-files-but-do-not-pass-any-filename-arguments
+   */
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
 };
